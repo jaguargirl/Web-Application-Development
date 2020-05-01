deed7185cb26facbed788ef8813beb12cea6fb62
natabobu <61291322+natabobu@users.noreply.github.com>
2020-05-01 18:39:57 +0300

    Add files via upload

---
+++ Application12 - SpringRESTfulDeploy/src/main/java/nata/daw/domain/CarRepository.java
@@ -0,0 +1,22 @@
+package nata.daw.domain;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+    List<Car> findByBrand(@Param("brand") String brand);
+    List<Car> findByColor(@Param("color") String color);
+    List<Car> findByYear(@Param("year") int year);
+    List<Car> findByBrandAndModel(@Param("brand") String brand, @Param("model") String model);
+    List<Car> findByBrandOrColor(@Param("brand") String brand,@Param("color") String color);
+    List<Car> findByBrandOrderByYearAsc(@Param("brand") String brand);
+    /*
+    @Query("select c from Car c where c.brand like %?1")
+    List<Car> findByBrand(String brand);
+     */
+    @Query("select c from Car c where c.brand like %?1")
+    List<Car> findByBrandEndsWith(@Param("brand") String brand);
+}
