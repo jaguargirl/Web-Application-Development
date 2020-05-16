b5f56866f2de018f969f8f52208281ea712e0a65
natabobu <61291322+natabobu@users.noreply.github.com>
2020-05-16 16:05:43 +0300

    Update Application.java

--- Application10 - SpringSecurity/src/main/java/nata/daw/Application.java
+++ Application10 - SpringSecurity/src/main/java/nata/daw/Application.java
@@ -7,10 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
