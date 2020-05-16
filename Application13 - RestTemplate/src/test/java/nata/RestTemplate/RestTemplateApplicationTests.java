1c0b7b578614db82eefb361bab06d0117c1a7856
natabobu <61291322+natabobu@users.noreply.github.com>
2020-05-16 18:28:09 +0300

    Add files via upload

---
+++ Application13 - RestTemplate/src/test/java/nata/RestTemplate/RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package nata.RestTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
